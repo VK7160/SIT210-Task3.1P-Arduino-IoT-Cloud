98fde29ab55e89569694c671a7aa66d2338ea549
Vansh Khanna <74200673+vanshk313@users.noreply.github.com>
2021-09-24 23:13:11 +0530

    Update thingProperties.h

--- thingProperties.h
+++ thingProperties.h
@@ -10,14 +10,12 @@ const char SSID[]     = SECRET_SSID;    // Network SSID (name)
 const char PASS[]     = SECRET_PASS;    // Network password (use for WPA, or use as key for WEP)
 
 
-bool Led;
-float temp;
+float Temperature;
 
 void initProperties(){
 
   ArduinoCloud.setThingId(THING_ID);
-  ArduinoCloud.addProperty(Led, READ, ON_CHANGE, NULL);
-  ArduinoCloud.addProperty(temp, READ, ON_CHANGE, NULL);
+  ArduinoCloud.addProperty(Temperature, READ, ON_CHANGE, NULL);
 
 }
 
